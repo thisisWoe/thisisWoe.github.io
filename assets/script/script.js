fff0f92c6142e4d49763062fd58c42dc83a15205
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-05-03 23:36:30 +0200

    alcuni fix

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,7 +4,13 @@ function openModal() {
     let myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {});
     myModal.show();
   }
-
+//da togliere poi===============================
+function redirectToPage(url) {
+    setTimeout(() => {
+      window.location.href = url;
+    }, 2000);
+  }
+//==============================================  
 const languageModal = document.getElementById('staticBackdrop');
 const fadedEffect = document.getElementsByClassName('modal-backdrop');
 const italianButton = document.getElementById('italian');
@@ -28,17 +34,27 @@ const englishButton = document.getElementById('english');
 //distruggo la modale e creo la pagina
 italianButton.addEventListener('click', function () {
     languageModal.remove();
-    document.body.innerHTML = '';
+    const bodyChildren = document.querySelectorAll("#body>div");
+    bodyChildren.forEach(function(element) {
+        element.remove();
+    }); //distruggo ogni figlio di body
     //2 righe di prova
     let myBody = document.getElementById('body');
     myBody.classList.remove('modal-open');
     let myNewItemTest = document.querySelector('#body');
     createAndAppendSingleElement('h1','body', 'start', 'prova', 'h1');
     document.getElementById('prova').innerHTML = 'Ciao Mondo!';
+    redirectToPage('index-ita.html')
+    //DA TOGLIERE QUANDO FINISCO LA STRUTTURA DELLA PAGINA
+    
+      
 });
 englishButton.addEventListener('click', function () {
     languageModal.remove();
-    document.body.innerHTML = '';
+    const bodyChildren = document.querySelectorAll("#body>div");
+    bodyChildren.forEach(function(element) {
+        element.remove();
+    }); //distruggo ogni figlio di body
     //2 righe di prova
     let myBody = document.getElementById('body');
     myBody.classList.remove('modal-open');
